0b79f31b972c65e4d81bf73701eb7ee5d1eff487
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-06-05 15:15:48 +0900

    채팅구현 완료

---
+++ src/js/chat.js
@@ -0,0 +1,44 @@
+"use strict";
+const socket = io();
+
+const nickname = document.querySelector("#nickname");
+const chatlist = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
+sendButton.addEventListener("click", () => {
+  const param = {
+    name: nickname.value,
+    msg: chatInput.value,
+  };
+
+  socket.emit("chatting", param); // 채팅명
+});
+
+socket.on("chatting", (data) => {
+  const { name, msg, time } = data;
+  const item = new LiModel(name, msg, time);
+  item.makeLi();
+  displayContainer.scrollTo(0, displayContainer.scrollHeight);
+});
+
+function LiModel(name, msg, time) {
+  this.name = name;
+  this.msg = msg;
+  this.time = time;
+
+  this.makeLi = () => {
+    const li = document.createElement("li");
+    li.classList.add(nickname.value === this.name ? "sent" : "received");
+    const dom = `<span class="profile">
+    <span class="user">${this.name}</span>
+    <img class="image" src="https://placeimg.com/50/50/any" alt="any">
+    </span>
+    <span class="message">${this.msg}</span>
+    <span class="time">${this.time}</span>`;
+    li.innerHTML = dom;
+    chatlist.appendChild(li);
+  };
+}
+
+//emit 을 통해서 통신을 함
